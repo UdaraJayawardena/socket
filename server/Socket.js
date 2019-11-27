f0bea70c9e5582f6435880dd7785048c6ce13a3d
UdaraJayawardena <udarajaywardena5@gmail.com>
2019-11-28 00:39:45 +0530

    socket updated

--- server/Socket.js
+++ server/Socket.js
@@ -1,74 +1,111 @@
 const express = require("express");
+const axios = require('axios');
 const SocketIO = require('socket.io').listen(5050, { secure: true }).sockets;
-var app = express();
+const NodeCache = require('node-cache');
 
-app.get('/data', ((req, res) => {
-    res.json('Hello Server')
-}))
+let driverarray = [];
+let passengerarray = [];
 
-app.listen('8080', () => console.log('8080 Running'));
+let driverCache = new NodeCache();
+let passengerCache = new NodeCache();
+var arrNew = [];
 
 SocketIO.on('connection', function (socket) {
-    console.log('connected')
+
+    console.log('client connected');
+
+
     //Disconnect from Server
-
-    socket.emit('fromPassenger', {
-        username: "Udara",
-        message: "This is a Message from Udara"
-    });
-
-
     socket.on('disconnect', function () {
-        console.log('client disconnect from server');
+        console.log('client disconnect');
     });
-
 
     socket.on('lookingForPassengers', driverResponse => {
 
-        console.log(driverResponse)
         var driverobj = {
             "driverID": driverResponse.driverID,
             "driverSocketID": driverResponse.driverSocketID,
             "driverVehicleType": driverResponse.driverVehicleType
         }
-        driverarray.push(driverobj);
+        // driverarray.push(driverobj);
+        driverCache.set(driverResponse.driverID, driverobj);
+        var arr = []
+
+        arr.push(driverCache.get(driverResponse.driverID));
+        // console.log(driverarray);
+
     })
 
     socket.on('lookingForDrivers', passengerResponse => {
 
-        console.log(passengerResponse);
         var passengerobj = {
             "passengerID": passengerResponse.passengerID,
             "passengerSocketID": passengerResponse.passengerSocketID,
             "startLatitude": passengerResponse.startLatitude,
             "startLongitude": passengerResponse.startLongitude,
             "endLatitude": passengerResponse.endLatitude,
-            "endLongitude": passengerResponse.endLongitude
+            "endLongitude": passengerResponse.endLongitude,
+            "passengerVehicleType": passengerResponse.passengerVehicleType,
+            "passengerPlaceID": passengerResponse.passengerPlaceID
         }
+        // passengerarray.push(passengerobj);
 
-        passengerarray.push(passengerobj);
+        // console.log(passengerarray);
 
-        // if (driverArray.driverVehicleType === passengerArray.PassengerVehicleType) {
+        //Search Drivers Near to the Passenger
+        // this.searchDriver(passengerarray);
 
-        //     socket.broadcast.to(driverArray[driverArray.length - 1].driverSocketID).emit('ServerToDriver', passengerArray[passengerArray.length - 1]);
-
+        // if (driverarray.driverVehicleType === passengerarray.passengerVehicleType) {
+        //     socket.broadcast.to(driverarray[driverarray.length - 1].driverSocketID).emit('ServerToDriver', passengerarray[passengerarray.length - 1]);
         // } else {
         //     console.log('Vehicle Type Mismatch');
         // }
 
+        // passengerArray.push(passengerobj);
+        passengerCache.set(passengerResponse.passengerID, passengerobj);
+
+        for (var i = 0; i < driverCache.getStats().keys; ++i) {
+            arrNew.push(driverCache.get(i + 1));
+        }
+        for (var z = 0; z < arrNew.length; z++) {
+
+            socket.broadcast.to(arrNew[z].driverSocketID).emit('ServerToDriver', passengerCache.get(passengerResponse.passengerID));
+        }
+
     });
 
-  
+
 
     socket.on('driverAccept', driverData => {
-        console.log(driverData.passengerSocketID);
 
-        cron.schedule("* * * * * * * * *", function () {
-            socket.broadcast.to(driverData.passengerSocketID).emit('fromServer', driverData);
+        // axios.post('http://' + serverConfig.host + ':' + serverConfig.ongoing_trips + '/api/trips', {
+        //     "ongoing_trip_id": driverData.ongoing_trip_id,
+        //     "driver_id": driverData.driver_id,
+        //     "driver_socket_id": driverData.driver_socket_id,
+        //     "passenger_id": driverData.passenger_id,
+        //     "passenger_socket_id": driverData.passenger_socket_id,
+        //     "longitude": driverData.longitude,
+        //     "latitude": driverData.latitude,
+        //     "date": driverData.currentDate
+        // }).then((resss) => {
+        // });
 
-        });
+        socket.broadcast.to(driverData.passenger_socket_id).emit('fromServer', driverData);
+    });
 
+    socket.on('driverReconnect', driverReconnectResponse => {
+        axios.get('http://' + '192.168.1.12' + ':' + '3080' + '/api/trips?filter={"where":{"driver_id":"' + driverReconnectResponse.driver_id + '"}}').then((ressDriverData) => {
+            socket.broadcast.to(ressDriverData[0].passenger_socket_id).emit('fromServer', ressDriverData);
+        })
+    })
+
+    socket.on('tripEnd', tripEndData => {
+        axios.get('http://' + '192.168.1.12' + ':' + '3080' + '/api/trips?filter={"where":{"ongoing_trip_id":"' + tripEndData.ongoing_trip_id + '"}}').then((ress) => {
+            console.log(ress);
+        })
     })
 
 
-})
+});
+
+
